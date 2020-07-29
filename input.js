846d9bdc3212102d9c3fcd10f042bad04a2831a4
micmor-m <micmor.m@gmail.com>
2020-07-29 18:26:26 +0000

    it's moving!

---
+++ input.js
@@ -0,0 +1,52 @@
+/**
+ * Setup User Interface 
+ * Specifically, so that we can handle user input via stdin
+ */
+// Stores the active TCP connection object.
+let connection; //this is oumost scope
+
+const setupInput = function(conn) {
+  connection = conn; //linked here
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+
+
+ // on any input from stdin (standard input), output a "." to stdout
+ stdin.on('data', function handleUserInput (key) {
+  //process.stdout.write('.');
+  // \u0003 maps to ctrl+c input
+  if (key === '\u0003') {
+    process.exit();
+  }
+
+  if (key === 'w') {
+    //console.log('Move: up');
+    conn.write('Move: up');
+  }
+  if (key === 'a') {
+    //console.log('Move: left');
+    connection.write('Move: left');
+  }
+  if (key === 's') {
+    //console.log('Move: down');
+    connection.write('Move: down');
+  }
+  if (key === 'd') {
+    //console.log('Move: right');
+    connection.write('Move: right');
+  }
+  
+
+
+  })
+
+  
+
+  return stdin;
+  }
+
+module.exports = setupInput;
+
+
--- play.js
+++ play.js
@@ -1,33 +1,7 @@
 
 const connect = require('./client.js');
+const setupInput = require('./input.js');
 
 console.log('Connecting ...');
-connect();
 
-
-/**
- * Setup User Interface 
- * Specifically, so that we can handle user input via stdin
- */
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-
- // on any input from stdin (standard input), output a "." to stdout
- stdin.on('data', function handleUserInput (key) {
-  //process.stdout.write('.');
-  // \u0003 maps to ctrl+c input
-  if (key === '\u0003') {
-    process.exit();
-  }
-  })
-
-
-  return stdin;
-  }
-
-  
-
-setupInput();
+setupInput(connect());
